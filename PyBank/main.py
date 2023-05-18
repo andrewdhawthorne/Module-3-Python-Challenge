90263624390f6770ecd0133f0e08bcc669e5b759
andrewdhawthorne <adhawthorne@gmail.com>
2023-05-17 20:25:40 -0500

    Working on homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,12 +5,10 @@ import csv
 dates = []
 monthly_total = []
 total_months = 0
-# net_total = 0
-# greatest_increase = 0
-# current_month = 0
-# next_month = 0
-# greatest_decrease = 0
-# overall_change = 0
+net_total = 0
+greatest_increase = 0
+greatest_decrease = 0
+overall_change = 0
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 print(csvpath)
@@ -20,49 +18,68 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     csv_header = next(csvreader)
-    changes_list = []
-    first_row = next(csvreader)
-    # print(first_row) => ['date', '1']
-    # prev_profit = int(first_row[1])
+
     print(f"CSV Header: {csv_header}")
-    # total = int(first_row[1])...
-
-
 
     for row in csvreader:
-       # calculate the change using the prev_profit
-       # append that to your list
-       cur_date = row[0]
-       cur_profit = int(row[1])
-        #  row = ['Jan-10','1088983']
        dates.append(row[0])
        monthly_total.append(int(row[1]))
-    # chonges_list now equals [1, -2, 3]
-    # avg_change = sum(changes_list) / len(changes_list)
+
+       if int(row[1]) > greatest_increase:
+           greatest_increase = int(row[1])
+    
 # find total months 
 for i in dates:
     total_months += 1
 
-# loop through profits to find greatest increase
-for i in monthly_total:
-    current_month = int(i)
-    next_month = int(next(i))
-    monthly_change = next_month - current_month
-    overall_change += monthly_change   
-
-#average_change = overall_change / total_months    
-
-#print(average_change)
-
-
 # find total profits/losses
-# for i in monthly_total:
-    # net_total += int(i)
+for i in monthly_total:
+    net_total += int(i)
 
+print(monthly_total)
+print(total_months)
+
+start_total = int(monthly_total[0])
+end_total = int(monthly_total[-1])
+overall_change = end_total - start_total
+
+print(overall_change)
+
+average_change = overall_change/(total_months -1)
+average_change = round(average_change, 2)
+
+print(average_change)
+
+monthly_changes_list = []
+month_1 = int(monthly_total[0])
+month_2 = 0
+monthly_change = 0
+greatest_increase_date = ""
+greatest_decrease_date = ""
+
+for i in monthly_total:
+    month_2 = int(i)
+    monthly_change = month_2 - month_1
+    month_1 = month_2
+    monthly_changes_list.append(monthly_change)
+
+greatest_increase = max(monthly_changes_list)
+greatest_decrease = min(monthly_changes_list)
+
+for i in range(1, len(monthly_changes_list)): 
+    if monthly_changes_list[i] == greatest_increase:
+        greatest_increase_date = dates[i]
+    elif monthly_changes_list[i] == greatest_decrease:
+        greatest_decrease_date = dates[i]
 
 
 # display results 
-#print("Financial Analysis\n")
-#print("-------------------\n")
+print("Financial Analysis\n")
+print("-------------------\n")
 print(f"Total Months: {total_months}\n")
-#print(f"Total: ${net_total}\n")
+print(f"Total: ${net_total}\n")
+print(f"Average Change: ${average_change}\n")
+print(f"Greatest Increase in Profits: ${max(monthly_changes_list)}\n")
+print(f"Greatest Decrease in Profits: {greatest_increase_date} ${min(monthly_changes_list)}\n")
+print(greatest_increase_date)
+print(greatest_decrease_date)
