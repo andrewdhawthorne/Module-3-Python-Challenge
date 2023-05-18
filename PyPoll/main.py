1e69aae2c2ce500fb4d7b2314f51bfe5181d376f
andrewdhawthorne <adhawthorne@gmail.com>
2023-05-18 18:19:13 -0500

    Adding dictionary

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,15 +1,16 @@
 import os
 import csv
 
-
-CSV_PATH = os.path.join('Resources', 'election_data.csv')
-CANDIDATE_IDX = 2
+csv_path = os.path.join('Resources', 'election_data.csv')
+candidate_name = row[2] 
+candidate_list = []
+candidate_vote_count = {}
 
 total_votes = 0
 
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-with open(CSV_PATH) as csv_file:
+with open(csv_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     csv_header = next(csv_reader)
     #print(f"CSV Header: {csv_header}")
@@ -18,9 +19,9 @@ with open(CSV_PATH) as csv_file:
         # Calculate total votes 
         total_votes += 1
         # Calcuatle total votes per candidate 
-
+candidate_vote_count[candidate_name] = candidate_vote_count[candidate_name] + 1
     # Calculate the percentage per candidate 
-
+candidate_vote_count[candidate_name] = 1
     # Identify which candidate had the most votes 
-
+print(candidate_vote_count)
 print(total_votes)
